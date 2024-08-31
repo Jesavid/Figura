f07b21a1bb76e9eb1e564cb673e5a2d24cc85b3c
Jesus David <jesusdavida.e2089@gmail.com>
2024-08-31 14:42:11 -0700

    Modificacion del menu

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from Cuadrado import Cuadrado
-
+from Circulo import Circulo
 def main():
     cuadrado = Cuadrado()
+    circulo = Circulo()
 
-    lado = int(input("El lado del cuadrado es: "))
-    cuadrado.set_lado(lado)
-    print(cuadrado.areaCuadrado())
-    print(cuadrado.perimetroCuadrado())
+    print("1. Cuadrado")
+    print("2. Circulo")
+    print("3. Triángulo")
+    seleccion = int(input("¿Que desea hacer? "))
+
+    if (seleccion == 1):
+        lado = int(input("El lado del cuadrado es: "))
+        cuadrado.set_lado(lado)
+        print(f"El área del cuadrado es: {cuadrado.areaCuadrado()}")
+        print(f"El perimetro del cuadrado es: {cuadrado.perimetroCuadrado()}")
+
+    elif (seleccion == 2):
+        radio = int(input("El radio del ciruclo es: "))
+        circulo.set_radio(radio)
+        print(f"El área del circulo es: {circulo.areaCirculo()}")
+
+
+
 
 if __name__ == '__main__':
     main()
